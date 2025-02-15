6f84b1fd0dea2c594c5e980643a5bc1409cbca86
Matheus Arakaki <arakakimath@gmail.com>
2025-02-14 21:30:37 -0400

    Adding zod for validating environmental variables and eslint for code formatting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { HTTPModule } from './http/http.module';
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { HTTPModule } from './http/http.module'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './infra/env/env'
+import { EnvService } from './infra/env/env.service'
+import { EnvModule } from './infra/env/env.module'
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://mongodb:docker@mongo:27017/order-logistics?authSource=admin'),
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [EnvModule],
+      inject: [EnvService],
+      useFactory(env: EnvService) {
+        const databaseURL = env.get('DATABASE_URL')
+
+        return {
+          uri: databaseURL,
+        }
+      },
+    }),
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
     HTTPModule,
   ],
+  providers: [EnvService],
 })
 export class AppModule {}
