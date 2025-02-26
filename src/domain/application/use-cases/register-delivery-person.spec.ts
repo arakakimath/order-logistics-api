47631664eceefaac57f746f035f3d3441f21ac2e
Matheus Arakaki <arakakimath@gmail.com>
2025-02-26 13:42:37 -0400

    test: ensure password hashing

--- src/domain/application/use-cases/register-delivery-person.spec.ts
+++ src/domain/application/use-cases/register-delivery-person.spec.ts
@@ -1,14 +1,21 @@
 import { InMemoryDeliveryPeopleRepository } from 'test/in-memory-repositories/delivery-people.repository'
 import { RegisterDeliveryPersonUseCase } from './register-delivery-person'
+import { FakeHasher } from 'test/cryptography/fake-hasher'
+import { makeDeliveryPerson } from 'test/factories/make-delivery-person'
 
-let deliveryPersonRepository: InMemoryDeliveryPeopleRepository
+let deliveryPeopleRepository: InMemoryDeliveryPeopleRepository
+let fakeHasher: FakeHasher
 let sut: RegisterDeliveryPersonUseCase
 
 describe('Register Delivery Person', () => {
   beforeEach(() => {
-    deliveryPersonRepository = new InMemoryDeliveryPeopleRepository()
+    deliveryPeopleRepository = new InMemoryDeliveryPeopleRepository()
+    fakeHasher = new FakeHasher()
 
-    sut = new RegisterDeliveryPersonUseCase(deliveryPersonRepository)
+    sut = new RegisterDeliveryPersonUseCase(
+      deliveryPeopleRepository,
+      fakeHasher,
+    )
   })
 
   it('should be able to register a delivery person', async () => {
@@ -22,10 +29,23 @@ describe('Register Delivery Person', () => {
     if (result.isLeft()) throw new Error()
     expect(result.value?.deliveryPerson.id).toBeTruthy()
     expect(result.value?.deliveryPerson.isAdmin()).toBeFalsy()
-    expect(deliveryPersonRepository.items[0]).toEqual(
+    expect(deliveryPeopleRepository.items[0]).toEqual(
       expect.objectContaining({
         name: 'John Doe',
       }),
     )
+  })
+
+  it(`should be able to hash user's password`, async () => {
+    const deliveryPerson = makeDeliveryPerson()
+
+    console.log(deliveryPerson.cpf)
+
+    const result = await sut.execute(deliveryPerson)
+
+    const hashedPassword = await fakeHasher.hash(deliveryPerson.password)
+
+    expect(result.isRight()).toBeTruthy()
+    expect(deliveryPeopleRepository.items[0].password).toEqual(hashedPassword)
   })
 })
