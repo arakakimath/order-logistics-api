8d0f6c051c0031f96213d82298e71f789532176f
Matheus Arakaki <arakakimath@gmail.com>
2025-02-26 15:23:16 -0400

    test: cpf validation

--- src/core/validation/cpf.validation.spec.ts
+++ src/core/validation/cpf.validation.spec.ts
@@ -1,11 +1,15 @@
 import { CpfValidation } from './cpf.validation'
 
 describe('CPF Validation', () => {
-  it('should be able to get the last two digits from a CPF', () => {
+  it('should be able to validate a valid CPF', () => {
     const cpf = '097.554.991-05'
 
-    expect(CpfValidation.getCpfLastDigits(cpf.split('-')[0])).toEqual(
-      Number(cpf.split('-')[1]),
-    )
+    expect(CpfValidation.isCpfValid(cpf)).toBeTruthy()
+  })
+
+  it('should not be able to validate an invalid CPF', () => {
+    const cpf = '097.554.921-05'
+
+    expect(CpfValidation.isCpfValid(cpf)).toBeFalsy()
   })
 })
