0d0e6198f8a536a7a1b0746c34652c9f04b5f4fa
Matheus Arakaki <arakakimath@gmail.com>
2025-02-28 23:06:35 -0400

    test: feat authentication controller

---
+++ src/infra/http/controllers/authenticate.controller.e2e-spec.ts
@@ -0,0 +1,45 @@
+import { AppModule } from '@/app.module'
+import request from 'supertest'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { DeliveryPersonFactory } from 'test/factories/make-delivery-person'
+import { hash } from 'bcryptjs'
+import { DatabaseModule } from '@/infra/database/database.module'
+
+describe('Authenticate delivery person (e2e)', () => {
+  let app: INestApplication
+  let deliveryPersonFactory: DeliveryPersonFactory
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [DeliveryPersonFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    deliveryPersonFactory = moduleRef.get(DeliveryPersonFactory)
+
+    await app.init()
+  })
+
+  test('[POST] /auth', async () => {
+    const deliveryPerson =
+      await deliveryPersonFactory.makeMongooseDeliveryStudent({
+        password: await hash('123456', 8),
+      })
+
+    const response = await request(app.getHttpServer()).post('/auth').send({
+      cpf: deliveryPerson.cpf,
+      password: '123456',
+    })
+
+    expect(response.statusCode).toBe(200)
+
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        access_token: expect.any(String),
+      }),
+    )
+  })
+})
