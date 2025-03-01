635c9a70fddbb6d6eafb5e4540b2474a8ca47034
Matheus Arakaki <arakakimath@gmail.com>
2025-02-28 23:03:24 -0400

    test: mongodb-memory-server for e2e tests

--- test/setup.e2e.ts
+++ test/setup.e2e.ts
@@ -1,5 +1,18 @@
 import { config } from 'dotenv'
+import { MongoMemoryServer } from 'mongodb-memory-server'
 
 config({ path: '.jwt.env', override: true })
 config({ path: '.env', override: true })
 config({ path: '.env.test', override: true })
+
+let mongoServer: MongoMemoryServer
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create()
+  const dbName = `test_db_${Date.now()}`
+  process.env.DATABASE_URL = mongoServer.getUri(dbName)
+})
+
+afterAll(async () => {
+  await mongoServer.stop()
+})
