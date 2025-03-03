c516ba2b4d665858cc976d3e1827f718f17f8106
Matheus Arakaki <arakakimath@gmail.com>
2025-03-03 15:46:33 -0400

    test: add authentication step

--- src/infra/http/controllers/create-user.controller.e2e-spec.ts
+++ src/infra/http/controllers/create-user.controller.e2e-spec.ts
@@ -3,40 +3,62 @@ import { MongooseService } from '@/infra/database/mongoose/mongoose.service'
 import request from 'supertest'
 import { INestApplication } from '@nestjs/common'
 import { Test } from '@nestjs/testing'
-import { makeDeliveryPerson } from 'test/factories/make-delivery-person'
+import {
+  DeliveryPersonFactory,
+  makeDeliveryPerson,
+} from 'test/factories/make-delivery-person'
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from '@/infra/env/env'
+import { JwtService } from '@nestjs/jwt'
+import { DatabaseModule } from '@/infra/database/database.module'
 
 describe('Register delivery person (e2e)', () => {
   let app: INestApplication
   let mongoose: MongooseService
+  let deliveryPersonFactory: DeliveryPersonFactory
+  let jwt: JwtService
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [
         AppModule,
+        DatabaseModule,
         ConfigModule.forRoot({
           validate: (env) => envSchema.parse(env),
           isGlobal: true,
         }),
       ],
+      providers: [DeliveryPersonFactory],
     }).compile()
 
     app = moduleRef.createNestApplication()
 
     mongoose = moduleRef.get(MongooseService)
+    deliveryPersonFactory = moduleRef.get(DeliveryPersonFactory)
+    jwt = moduleRef.get(JwtService)
 
     await app.init()
   })
 
   test('[POST] /users', async () => {
+    const deliveryPerson =
+      await deliveryPersonFactory.makeMongooseDeliveryStudent({ admin: true })
+
+    const accessToken = jwt.sign({
+      sub: deliveryPerson.id.toString(),
+      role: deliveryPerson.isAdmin,
+    })
+
     const { cpf } = makeDeliveryPerson()
 
-    const response = await request(app.getHttpServer()).post('/users').send({
-      name: 'John Doe',
-      cpf,
-      password: '123456',
-    })
+    const response = await request(app.getHttpServer())
+      .post('/users')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        name: 'John Doe',
+        cpf,
+        password: '123456',
+      })
 
     expect(response.statusCode).toBe(201)
 
