8d0bb32b35b61b48a95fcb7f9c4ee2fd1d620967
Matheus Arakaki <arakakimath@gmail.com>
2025-03-11 16:32:28 -0400

    feat: create and findByID methods for in memory orders repository

---
+++ test/in-memory-repositories/orders.repository.ts
@@ -0,0 +1,22 @@
+import { OrdersRepository } from '@/domain/application/repositories/orders.repository'
+import { Order } from '@/domain/enterprise/entities/order'
+
+export class InMemoryOrdersRepository implements OrdersRepository {
+  public items: Order[] = []
+
+  async create(order: Order): Promise<void> {
+    this.items.push(order)
+  }
+
+  async save(order: Order): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+
+  async delete(order: Order): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+
+  async findByID(id: string): Promise<Order | null> {
+    return this.items.filter((item) => item.id.toString() === id)[0]
+  }
+}
